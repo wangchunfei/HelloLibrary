a17a05002c24aab397a3c27fb25653749156b98b
wcf <263022969@qq.com>
2017-06-08 15:13:06 +0800

    Initial commit

---
+++ library/src/main/java/com/wcf/library/CustomUtil.java
@@ -0,0 +1,13 @@
+package com.wcf.library;
+
+/**
+ * @author wcf
+ * @time 2017/6/8 13:44
+ * @desc
+ */
+
+public class CustomUtil {
+    public static String getMyHero(){
+        return "wcfwowowo";
+    }
+}
